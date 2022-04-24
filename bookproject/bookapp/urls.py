10bd7907479f431d7390c815f7e2745baa4471d9
bhu-maker <bhu_ess@yahoo.co.in>
2022-04-25 02:42:24 +0530

    book

--- bookproject/bookapp/urls.py
+++ bookproject/bookapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [path('',views.creating),
               path('list',views.listing),
               path('read/<int:pk>',views.reading),
               path('up/<int:pk>',views.updating),
-              path('del/<int:pk>',views.deleting)]
+              path('del/<int:pk>',views.deleting),
+              path('api/gett',views.gettingall),
+              path('api/postt',views.postting),
+              path('api/gett/<str:pk>',views.readgetting)]
